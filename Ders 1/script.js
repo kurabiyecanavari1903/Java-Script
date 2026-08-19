2002beff31c4162a00d7d33597e1a6f7192ed727
kurabiyecanavari1903 <bilalozturk049@gmail.com>
2026-08-19 19:10:01 +0300

    first commit

---
+++ Ders 1/script.js
@@ -0,0 +1,12 @@
+let x = "10"; // Lette ilk değişkene(x) verilen değerden sonra altına aynı değişkene başka bi değer atansaydı x değeri değişebilirdi!
+let y = "ahmet";
+let z = 5;
+
+console.log(x);
+console.log(y);
+console.log(x + z); // 105 vermesinin sebebi 10'u stringli 5'i normal yazmamız ikisi de aynı olsa toplamı 15 olurdu!
+// const y = 15; Constta ilk yazılan değer sabit kalır, değiştiremezsin!
+// y = 12;
+
+// console.log(x);
+// console.log(y);
---
+++ Ders 2/script.js
@@ -0,0 +1,16 @@
+// String
+let text = "koray merhaba!"
+
+// Number
+let number = 1239021841;
+
+// Boolean
+let x = true;
+let y = false;
+
+// Null
+let xyz = null; // null = bilerek boş bırakılmış, içine birşey yazılmamış
+
+
+// Undefined
+let asd; // undefined = hiçbir değer girilmemiş, yazılımcı bilerek boş bırakmamış
